68d6775de17f681e3282ad7467702ed6f5de6251
Sch <junhg0211@gmail.com>
2022-12-14 01:02:21 +0900

    마우스가 있는 네마 표시

--- static/js/hjulien.js
+++ static/js/hjulien.js
@@ -1,11 +1,49 @@
 let hjulien;
 let hjulienCtx;
 
-let hjluienDirection = false;
+let hjulienDirection = false;
+
+let mouseX, mouseY;
+
+let unit;
+
+let floatX, floatY;
+
+let nemas = {};
+
+function tick() {
+    floatX = Math.round((mouseX/unit - 3.5) / 2);
+    floatY = Math.round((mouseY/unit - 3.5) / 2);
+}
+
+function drawHighlightNema() {
+    let width = unit;
+    let height = 3 * unit;
+    if ((floatX + floatY) % 2 === (hjulienDirection ? 1 : 0)) {
+        [width, height] = [height, width];
+    }
+    let x = 2 * unit * (floatX + 1.75) - width / 2;
+    let y = 2 * unit * (floatY + 1.75) - height / 2;
+
+    hjulienCtx.strokeStyle = "#f7f7f9";
+    hjulienCtx.beginPath();
+    hjulienCtx.moveTo(x, y);
+    hjulienCtx.lineTo(x + width, y);
+    hjulienCtx.lineTo(x + width, y + height);
+    hjulienCtx.lineTo(x, y + height);
+    hjulienCtx.lineTo(x, y);
+    hjulienCtx.stroke();
+}
+
+function render() {
+    drawBackground();
+
+    if (0 <= floatX && floatX < 10 && 0 <= floatY && floatY < 10) {
+        drawHighlightNema();
+    }
+}
 
 function drawBackground() {
-    let unit = hjulien.width / 25;
-
     hjulienCtx.fillStyle = "#2c2c2c";
     hjulienCtx.fillRect(0, 0, hjulien.width, hjulien.height);
 
@@ -27,25 +65,25 @@ function drawBackground() {
 
     y = 2 * unit;
     for (let i = 0; i < 5; i++) {
-        x = (hjluienDirection ? 4 : 2) * unit + 4 * unit * i;
+        x = (hjulienDirection ? 4 : 2) * unit + 4 * unit * i;
         hjulienCtx.fillRect(x, y, 3 * unit, unit);
     }
 
     x = 2 * unit;
     for (let i = 0; i < 5; i++) {
-        y = (hjluienDirection ? 2 : 4) * unit + 4 * unit * i;
+        y = (hjulienDirection ? 2 : 4) * unit + 4 * unit * i;
         hjulienCtx.fillRect(x, y, unit, 3 * unit);
     }
 
     y = 22 * unit;
     for (let i = 0; i < 5; i++) {
-        x = (hjluienDirection ? 2 : 4) * unit + 4 * unit * i;
+        x = (hjulienDirection ? 2 : 4) * unit + 4 * unit * i;
         hjulienCtx.fillRect(x, y, 3 * unit, unit);
     }
 
     x = 22 * unit;
     for (let i = 0; i < 5; i++) {
-        y = (hjluienDirection ? 4 : 2) * unit + 4 * unit * i;
+        y = (hjulienDirection ? 4 : 2) * unit + 4 * unit * i;
         hjulienCtx.fillRect(x, y, unit, 3 * unit);
     }
 }
@@ -57,7 +95,19 @@ function loadHjulien() {
     hjulien.width = 800;
     hjulien.height = 800;
 
+    unit = hjulien.width / 25;
+
     drawBackground();
+
+    setInterval(() => {
+        tick();
+        render();
+    }, 1000 / 30);
 }
 
 document.addEventListener('DOMContentLoaded', loadHjulien);
+document.addEventListener('mousemove', e => {
+    let rect = hjulien.getBoundingClientRect();
+    mouseX = e.clientX - rect.left;
+    mouseY = e.clientY - rect.top;
+});
