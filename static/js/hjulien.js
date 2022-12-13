4c67ba728604d1885977c4c7888f2ea3a9352bd0
Sch <junhg0211@gmail.com>
2022-12-14 01:13:18 +0900

    hülien에 있는 모든 네마를 표시하는 기능 추가

--- static/js/hjulien.js
+++ static/js/hjulien.js
@@ -9,21 +9,28 @@ let unit;
 
 let floatX, floatY;
 
-let nemas = {};
+let nemas = [];
+let colors = {};
 
 function tick() {
     floatX = Math.round((mouseX/unit - 3.5) / 2);
     floatY = Math.round((mouseY/unit - 3.5) / 2);
 }
 
-function drawHighlightNema() {
+function nemaRect(xi, yi) {
     let width = unit;
     let height = 3 * unit;
-    if ((floatX + floatY) % 2 === (hjulienDirection ? 1 : 0)) {
+    if ((xi + yi) % 2 === (hjulienDirection ? 1 : 0)) {
         [width, height] = [height, width];
     }
-    let x = 2 * unit * (floatX + 1.75) - width / 2;
-    let y = 2 * unit * (floatY + 1.75) - height / 2;
+    let x = 2 * unit * (xi + 1.75) - width / 2;
+    let y = 2 * unit * (yi + 1.75) - height / 2;
+
+    return [x, y, width, height];
+}
+
+function drawHighlightNema() {
+    let [x, y, width, height] = nemaRect(floatX, floatY);
 
     hjulienCtx.strokeStyle = "#f7f7f9";
     hjulienCtx.beginPath();
@@ -35,12 +42,31 @@ function drawHighlightNema() {
     hjulienCtx.stroke();
 }
 
+function drawNemas() {
+    nemas.forEach(nema => {
+        let [xi, yi, id] = nema;
+        let color = colors[id];
+
+        let [x, y, width, height] = nemaRect(xi, yi);
+
+        hjulienCtx.fillStyle = color;
+        hjulienCtx.fillRect(x, y, width, height);
+    });
+
+    /*
+     * 네마를 표시하기 위해서는 `colors` 변수에 id에서 네마 색으로 매핑하는 값을 넣어야 합니다.
+     * 이후, `nemas` 변수에 네마의 위치와 네마를 놓은 플레이어의 id를 길이가 3인 리스트로 넣어주면 됩니다.
+     */
+}
+
 function render() {
     drawBackground();
 
     if (0 <= floatX && floatX < 10 && 0 <= floatY && floatY < 10) {
         drawHighlightNema();
     }
+
+    drawNemas();
 }
 
 function drawBackground() {
