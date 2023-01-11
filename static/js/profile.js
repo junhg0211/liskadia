c86d48a1d26e8cd383f4d8049d4c7127dd9d3f56
Sch <junhg0211@gmail.com>
2023-01-11 09:36:27 +0900

    프로필 페이지에 레이팅 기록 그래프 그림

--- static/js/profile.js
+++ static/js/profile.js
@@ -16,3 +16,57 @@ function getContrastRatio(luminance1, luminance2) {
 
   return (luminance1 + 0.05) / (luminance2 + 0.05);
 }
+
+function linearInterpolation(v, a1, b1, a2, b2) {
+  if (b1 - a1 === 0)
+    return a2;
+
+  return (b2 - a2) * (v - a1) / (b1 - a1) + a2;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  let canvas = document.querySelector('#rating-history');
+  if (canvas === null) return;
+
+  let context = canvas.getContext('2d');
+
+  fetch(`/ratings/${USER_ID}`)
+    .then(r => r.json())
+    .then(data => {
+      if (data.code !== 200) throw Error;
+
+      return data.history;
+    })
+    .then(history => {
+      let minTime = Infinity, maxTime = -Infinity;
+      let maxRating = 0, minRating = Infinity;
+
+      for (let i = 0; i < history.length; i++) {
+        history[i]['time'] = new Date(history[i]['time']);
+        let timeStamp = history[i]['time'].getTime();
+        minTime = Math.min(minTime, timeStamp);
+        maxTime = Math.max(maxTime, timeStamp);
+        maxRating = Math.max(maxRating, history[i]['rating']);
+        minRating = Math.min(minRating, history[i]['rating']);
+      }
+
+      // noinspection JSCheckFunctionSignatures
+      minTime = new Date(minTime);
+      maxTime = new Date(maxTime);
+
+      let notMoved;
+      context.beginPath();
+      history.forEach(row => {
+        let x = linearInterpolation(row['time'].getTime(), minTime, maxTime, 0, canvas.width);
+        let y = linearInterpolation(row['rating'], minRating, maxRating, canvas.height, 0);
+
+        if (notMoved) {
+          context.moveTo(x, y);
+          notMoved = false;
+        } else {
+          context.lineTo(x, y);
+        }
+      });
+      context.stroke();
+    });
+});
