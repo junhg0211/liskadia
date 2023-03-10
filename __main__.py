1aed45cb8b76275bfa0d8305398783987677b5a7
Sch <junhg0211@gmail.com>
2023-03-10 18:32:05 +0900

    Add ssl context on running

--- __main__.py
+++ __main__.py
@@ -550,4 +550,6 @@ def get_setting():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(
+        debug=True, host='0.0.0.0', port=80,
+        ssl_context=('./cert/server.crt', './cert/server.key'))
